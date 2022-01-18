8e9b88bdd0c5c7caeac3a44282212ef5d7bb3bb5
giantX99 <93807005+giantX99@users.noreply.github.com>
2022-01-18 13:02:09 -0600

    Delete Euler_Transform.m

--- Robotics/Euler_Transform.m
+++
@@ -1,8 +0,0 @@
-function Euler = Euler_Transform(Tt)
-
-     Euler = [Tt(1,1)    Tt(1,2)   Tt(1,3)   0
-              Tt(2,1)    Tt(2,2)   Tt(2,3)   0
-              Tt(3,1)    Tt(3,2)   Tt(3,3)   0
-              0          0         0         1];
-
-end
