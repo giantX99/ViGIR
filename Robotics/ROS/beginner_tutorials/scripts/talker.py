3f469a0293c62910ab57adac771d9db8eb08500b
giantX99 <93807005+giantX99@users.noreply.github.com>
2022-02-12 15:20:48 -0600

    Add files via upload

--- Robotics/ROS/beginner_tutorials/scripts/talker.py
+++ Robotics/ROS/beginner_tutorials/scripts/talker.py
@@ -2,6 +2,8 @@
 #Every Python Node will have this declaration at the top.
 #It makes sure that the script is executed as a Python script.
 
+#BEFORE RUNNING THE NODE USE "source ~/catkin_ws/devel/setup.bash"
+#THIS COMMAND MAKES DEVEL RECOGNIZE THE PACKAGE THAT THE NODE IS FROM.
 
 ## Simple talker demo that published std_msgs/Strings messages
 ## to the 'chatter' topic
@@ -42,10 +44,14 @@ def talker():
 #This while loop is a fairly standard rospy construct.
 #The loop keeps checking for rospy.is_shutdown() function.
 #It checks if the program should exit or not.
-#This loop also calls rospy.loginfo(str), which performs triple-duty: the messages get printed to screen, it gets written to the Node's log file, and it gets written to rosout.
+#This loop also calls rospy.loginfo(str), which performs triple-duty:
+# 1.the messages get printed to screen 
+# 2.it gets written to the Node's log file 
+# 3.it gets written to rosout.
 
-if __name__ == '__main__':
+if __name__ == '__main__': #Check if this file is running from itself or if someone import it from.
     try:
         talker()
     except rospy.ROSInterruptException:
         pass
+#This if statement works so we don't continue executing this node after the sleep.
