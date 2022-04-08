b66ac163c0ef9e909e8155bb135909e712ee07cb
giantX99 <93807005+giantX99@users.noreply.github.com>
2022-04-08 12:57:49 -0500

    a.2

--- Automation_Depth_Camera_System/LCD_Feedback.py
+++ Automation_Depth_Camera_System/LCD_Feedback.py
@@ -1,33 +1,36 @@
 import csv
+import time
 from RPLCD.i2c import CharLCD
 
-lcd = CharLCD('PCF8574T', ) #(i2c port expander, LCD address in memory) = initiates the class variable
+#CharLCD(i2c port expander, LCD address in memory) = initiates the class variable
 
 #RPLCD functions:
     #lcd.write_string('') -> writes (\n = new line, \r = go back to column 0 (row,0))
     #lcd.cursor_pos = (row, col) -> cursor position, when '= home()' go back to initial position
-    #lcd.clear() -> clean the display
+    #lcd.clear() -> clean the display, reset cursor position
     #lcd.close(clear = True) -> close() clears the connection
     #lcd.cursor_mode = ... -> 'hide' = no cursor displayed ; 'line' = cursor indicated by underline ; 'blink' = cursor is blinking square.
 
 #setup:
-lcd.clear()
-lcd.cursor_mode = 'hide'
-lcd.cursor_pos = (0,3)
-lcd.write_string('1234567890')
-lcd.cursor_pos = (1,3)
+def lcd_setup():
+    lcd = CharLCD('PCF8574T', )
+    lcd.clear()
+    lcd.cursor_mode = 'hide'
+    lcd.cursor_pos = (0,3)
+    lcd.write_string('1234567890')
+    lcd.cursor_pos = (1,3)
+    return lcd
 
-#reading file:
-#conditional statement: if camera x capture pic write Y at (1,x+2) 
-def read_file(camera_status):
-    file_name = input('Enter file name: ')
+#reading file: 
+def status_from_file(file_name):
+    camera_status = []
     suffix = '.csv'
-        
+
     if (file_name.endswith(suffix)):
         file_name = file_name
     else:
         file_name += suffix
-        
+
     try:
         csv_file = open(file_name, 'r')
         csv_reader = list(csv.reader(csv_file))
@@ -35,22 +38,32 @@ def read_file(camera_status):
         for i in csv_reader:
             camera_status.append(i[1])
         csv_file.close()
+        return camera_status
     except FileNotFoundError:
         print('File not found!')
-        
-
-def main():
-    camera_status = []
-    read_file(camera_status)
-
-    for status in camera_status:
-        lcd.cursor_pos = (1,status+2)
-        if status == '1':
-            print('camera['+ status +'] = Y')
-            lcd.write_string('Y')
-        else:
-            print('camera['+ status +'] = N')
-            lcd.write_string('N')
+        csv_file.close()
+        return None
+    
+#main:
+def lcd_feedback(csv_file):
+    lcd = lcd_setup()
+    camera_status = status_from_file(csv_file)
+    if camera_status is None :
+        lcd.clear()
+        lcd.write_string('File Error!\n\rTry again.')
+    else:
+        for status in camera_status:
+            if status == '1':
+                print('camera[{status}] = Y')
+                lcd.write_string('Y')
+            else:
+                print('camera[{status}] = N')
+                lcd.write_string('N')
+        if camera_status.count('0') is False:
+            time.sleep(4)
+            lcd.clear()
+            lcd.wirte_string('Capture Success!')
+    lcd.close(clear = True)
 
 if __name__ == "__main__":
-    main()
+    lcd_feedback(input())
--- Automation_Depth_Camera_System/test.py
+++ Automation_Depth_Camera_System/test.py
@@ -1,41 +1,49 @@
 import csv
+import time
 
-def read_file(camera_status):
-    while(True):
-        file_name = input('Enter file name: ')
-        suffix = '.csv'
+def status_from_file(file_name):
+    camera_status = []
+    suffix = '.csv'
+
+    if (file_name.endswith(suffix)):
+        file_name = file_name
+    else:
+        file_name += suffix
+
+    try:
+        csv_file = open(file_name, 'r')
+        csv_reader = list(csv.reader(csv_file))
+        csv_reader.sort(key=lambda csv_reader: csv_reader[0])
+        for i in csv_reader:
+            camera_status.append(i[1])
+        csv_file.close()
+        return camera_status
+    except FileNotFoundError:
+        print('File not found!')
+        csv_file.close()
+        return None
         
-        if (file_name.endswith(suffix)):
-            file_name = file_name
-        else:
-            file_name += suffix
-        
-        try:
-            csv_file = open(file_name, 'r')
-            csv_reader = list(csv.reader(csv_file))
-            csv_reader.sort(key=lambda csv_reader: csv_reader[0])
-            for i in csv_reader:
-                camera_status.append(i[1])
-            #print(camera_status)
-            csv_file.close()
-            break
-        except FileNotFoundError:
-            print('File not found!')
-            continue
-
-def main():
-    while (True):
-        camera_status = []
-        read_file(camera_status)
-
+def lcd_feedback(csv_file):
+    #lcd = lcd_setup()
+    camera_status = status_from_file(csv_file)
+    if camera_status is None :
+        #lcd.clear()
+        #lcd.write_string('File Error!\n\rTry again.')
+        print('File Error!\nTry again.')
+    else:
         for status in camera_status:
-            #lcd.cursor_pos = (1,i+2)
             if status == '1':
-                print('camera['+ status +'] = Y')
+                print('camera[{status}] = Y')
                 #lcd.write_string('Y')
             else:
-                print('camera['+ status +'] = N')
+                print('camera[{status}] = N')
                 #lcd.write_string('N')
+        if camera_status.count('0') is False:
+            time.sleep(4)
+            print('Capture Successful!')
+            #lcd.clear()
+            #lcd.wirte_string('Capture Success!')
+    #lcd.close(clear = True)
 
 if __name__ == "__main__":
-    main()
+    lcd_feedback(input())
